5cd3f9b370b192a3d7c6bdb468602a6b0974b06f
Zivin30 <91950641+Zivin30@users.noreply.github.com>
2025-08-07 09:16:01 +0700

    Update 99.php

--- 99.php
+++ 99.php
@@ -1,6 +1,6 @@
 <?php
 define('host',['99MakeMoneyonline','99makemoneyonline.com','']);
-define('version','1.0.2');
+define('version','1.0.4');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
